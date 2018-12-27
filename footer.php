e99f11185f567548e7d52b40238d96ad9751a533
unknown <furqancsit@gmail.com>
2018-12-27 13:48:16 +0500

    changes

--- footer.php
+++ footer.php
@@ -1,3 +1,32 @@
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6 col-sm-6">
+				<div class="footer-link">
+					<ul>
+						<li><a href="#">SHOP</a></li>
+						<li><a href="#">OUTLET</a></li>
+						<li><a href="#">CUSTOM CAKES</a></li>
+						<li><a href="#">DEALS</a></li>
+						<li><a href="#">ABOUT</a></li>
+						<li><a href="#">CONTACT</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-lg-6 col-sm-6">
+				<div class="footer-socail-link">
+					<ul>
+						<li><p><a href="#">Austin, TX, United States, +1 512-491-8456</a></p></li>
+						<li><a href="#"> FACEBOOK</a></li>
+						<li><a href="#"> INSTAGRAM</a></li>
+						<li><a href="#"> PINTREST</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script type="text/javascript" src="js/jquery-3.3.1.slim.min.js"></script>
--- index.php
+++ index.php
@@ -11,17 +11,46 @@
   </div>
 
   <div class="container">
-    <div class="row">
+    <div class="row align-items-center h-100">
       <div class="col-lg-6 col-sm-6">
         <img src="images/heaven-birthdaycake.png" class="img-fluid">
       </div>
       <div class="col-lg-6 col-sm-6">
-         <h3>Austin, TX, United States, +1 512-491-8456</h3>
-         <h2>We’ve opned a bakery in Texas.</h2>
-         <a href="#" class="btn btnprim">CHECK US OUT</a>
+         <h3>We Make Custom Cakes</h3>
+         <h2>Need cakes for a birthday <br/>or other special events? </h2>
+         <a href="#" class="btn btnprim">ORDER CAKE</a>
       </div>
     </div>
   </div>
+
+  
+  <div class="container">
+    <div class="american-biscotta">
+      <div class="row align-items-center h-100">      
+        <div class="col-lg-6 col-sm-6">
+          <h4>. DINE IN DEAL .</h4>
+           <h1>Americano Biscatto</h1>
+           <p>Our finest blend of coffee served with <br/>2 freshly backed chocolate chip cookies.</p>
+           <h5>$12 - Available from 9am-12pm</h5>
+        </div>
+        <div class="col-lg-6 col-sm-6">
+          <img src="images/heaven-biscatto.png" class="img-fluid">
+        </div>
+      </div>
+    </div>
+  </div>
+  <a href="#" class="btn btnprim imgtb50">SEE ALL DEALS</a>  
+
+<div class="getintouch">
+  <div class="container">  
+         <h3>Talk To Us</h3>
+         <h2>Get in touch for details and inquiry.</h2>
+         <p>We are quick to respond and always here to help you out.</p>
+         <a href="#" class="btn btnprim">CONTACT US</a>
+  </div>
+</div>
+
+
 </div>
 
 
